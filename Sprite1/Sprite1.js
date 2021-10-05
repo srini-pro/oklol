2ba422fd8a11ab7f1ac56f7e58d0e867694f7da9
srini-pro <75109032+srini-pro@users.noreply.github.com>
2021-10-05 10:11:27 +0530

    Initial commit

---
+++ Sprite1/Sprite1.js
@@ -0,0 +1,30 @@
+/* eslint-disable require-yield, eqeqeq */
+
+import {
+  Sprite,
+  Trigger,
+  Costume,
+  Color,
+  Sound
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class Sprite1 extends Sprite {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume("costume1", "./Sprite1/costumes/costume1.svg", {
+        x: 48,
+        y: 50
+      }),
+      new Costume("costume2", "./Sprite1/costumes/costume2.svg", {
+        x: 46,
+        y: 53
+      })
+    ];
+
+    this.sounds = [new Sound("Meow", "./Sprite1/sounds/Meow.wav")];
+
+    this.triggers = [];
+  }
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+import { Project } from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+import Stage from "./Stage/Stage.js";
+import Sprite1 from "./Sprite1/Sprite1.js";
+
+const stage = new Stage({ costumeNumber: 1 });
+
+const sprites = {
+  Sprite1: new Sprite1({
+    x: 0,
+    y: 0,
+    direction: 90,
+    costumeNumber: 1,
+    size: 100,
+    visible: true
+  })
+};
+
+const project = new Project(stage, sprites);
+export default project;
